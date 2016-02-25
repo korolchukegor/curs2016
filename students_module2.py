aab2a1cf5079142c1592aa2515b22526a783cddd
egorkorolchuk <egorkorolchuk@gmail.com>
2016-02-25 12:43:25 +0300

    Модуль программы про студентов (с использованием try except)

--- students_module2.py
+++ students_module2.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 def func_2(s):
 	num = int(input('Введите индекс студента'))
-	s1 = s[num - 1]
-	print(s1)
-	return s1
+	try:
+		s1 = s[num - 1]
+		print(s1)
+	except IndexError:
+		print('Индекс не подходит')
+		func_2(s)
