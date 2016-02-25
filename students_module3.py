cdeca61fc0405985bd56c5e136645dfd91752361
egorkorolchuk <egorkorolchuk@gmail.com>
2016-02-25 12:54:40 +0300

    Модуль программы про студентов (с использованием try except)

--- students_module3.py
+++ students_module3.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 def func_3(s):
-	num2 = int(input('Введите начало среза списка студентов\n'))
-	num3 = int(input('Введите конец среза списка студентов\n'))
-	print(s[num2 - 1:num3])
-	return s[num2 - 1:num3]
+	try:
+		num2 = int(input('Введите начало среза списка студентов\n'))
+		num3 = int(input('Введите конец среза списка студентов\n'))
+		if num3 <= len(s):
+			print(s[num2 - 1:num3])
+		else:
+			print('Срез вне списка')
+	except:
+		print('некорректное значение')
