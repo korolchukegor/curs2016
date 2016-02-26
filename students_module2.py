6e362265acdc6eb455c7f82e8e79c7ff8c5200c2
korolchukegor <egorkorolchuk@gmail.com>
2016-02-26 19:47:19 +0300

    Delete students_module2.py

--- students_module2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def func_2(s):
-	num = int(input('Введите индекс студента'))
-	try:
-		s1 = s[num - 1]
-		print(s1)
-	except IndexError:
-		print('Индекс не подходит')
-		func_2(s)
